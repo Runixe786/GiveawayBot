61f54f334243e6ae14f12caf326c8ee38ea9ad44
MaybeSahil <91938988+Runixe786@users.noreply.github.com>
2024-08-10 21:32:36 +0530

    Update and rename varfile.rename to varfile.py

---
+++ varfile.py
@@ -0,0 +1,33 @@
+# List of admin IDs 
+admins = [1285333094] 
+
+# Auto-abortion time (in seconds)
+auto_abort = 1*60*60
+
+# Send a registration reminder to the group after every 10 registrations (True/False)
+group_stats = True
+
+# Group ID (Always starts with -)
+group_id = -1001524089605
+
+# Channel ID (Always starts with -)
+channel_id = -1001699294770
+
+# Test group ID (Always starts with -)
+test_group_id = -1001645677229
+
+# Test channel ID (Always starts with -)
+test_channel_id = -1001932707380
+
+# Bot token (Get it from @BotFather)
+bot_token = "7481330473:AAHqGFkdbt5Yel00DFk6TFMo7fI2oQuYEkM" 
+
+# Database file name
+database = "database.fs" 
+
+# Test mode (True/False)
+test_mode = True
+
+# Bot username
+bot_username = "DroidGiveaways_bot"
+
